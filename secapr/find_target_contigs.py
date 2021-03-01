e0f5cc0bc6e7ea78df20b45abf1abde54931ab44
mftorres <mftorres27@gmail.com>
2021-03-01 08:43:31 +0100

    fixed get_list_contigs

--- secapr/find_target_contigs.py
+++ secapr/find_target_contigs.py
@@ -213,13 +213,9 @@ def get_list_of_valid_exons_and_contigs(exon_contig_dict,loci_with_issues,possib
             contig_names = exon_contig_dict[exon]
             if keep_bestbitscore_boolean:
                 contig_names = find_bestbitscore_contig(contig_names,blast_df)
-                valid_contig_names.append(str(contig_names).replace('>',''))
-                bitscore_kept = True
-                print('Keeping contig with highest blast bitscore')
-            else:
-                contig_names = find_longest_contig(contig_names,blast_df)
-                valid_contig_names.append(str(contig_names).replace('>',''))
-                print('Keeping longest contig')
+             else:
+                contig_names = find_longest_contig(contig_names,blast_df)               
+            valid_contig_names.append(str(contig_names).replace('>',''))
     print('Keeping contig with highest blast bitscore') if keep_bestbitscore_boolean else print('Keeping longest contig')
     return valid_contig_names
 
